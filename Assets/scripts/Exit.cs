f62c671c8eed8eeb6fd17bbc38586ba64487a850
cmaurolccc <48099019+cmaurolccc@users.noreply.github.com>
2019-04-02 13:56:33 -0400

    Added Exit button

--- Assets/scripts/Exit.cs
+++ Assets/scripts/Exit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Exit : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void ClickExit()
+    private void OnMouseDown()
     {
         Application.Quit();
     }
