320b665eb3ac1c479f38475c8bd9e1538a7e381b
cmaurolccc <48099019+cmaurolccc@users.noreply.github.com>
2019-04-04 12:22:48 -0400

    Scripted Hallway doors

---
+++ Assets/scripts/HallwayToBedroom2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HallwayToBedroom2 : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Bedroom2");
+    }
+}
