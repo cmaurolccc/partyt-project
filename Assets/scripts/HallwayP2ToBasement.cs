4766b36dd6bcd42dfe131183651b39c8a4c210b8
Kyrem13 <48100776+Kyrem13@users.noreply.github.com>
2019-04-04 14:55:38 -0400

    I did a thing - Josh

---
+++ Assets/scripts/HallwayP2ToBasement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HallwayP2ToBasement : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Basement");
+    }
+}
---
+++ Assets/scripts/ShedToOutside.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShedToOutside : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Outside");
+    }
+}
