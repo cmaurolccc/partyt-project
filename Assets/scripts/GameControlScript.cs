3fd97de8ed2ecb7def2a6213247bdf34b07cfdf3
cmaurolccc <48099019+cmaurolccc@users.noreply.github.com>
2019-04-16 14:02:05 -0400

    stupid shit that doesn't work

--- Assets/scripts/GameControlScript.cs
+++ Assets/scripts/GameControlScript.cs
@@ -32,16 +32,16 @@ public class GameControlScript : MonoBehaviour
     // this is the 2D array of the room map
     public int[,] roomGridArray =
        {
-            {2,4,3,5 },
-            {7,1,0,0 },
-            {0,0,0,1 },
-            {1,0,0,6 },
-            {0,0,1,0 },
-            {0,0,4,0 },
-            {0,2,8,0 },
-            {9,10,0,7 },
-            {0,8,0,0 },
-            {8,0,0,0 }
+            {0,0,2,0 }, //room 1 bedroom
+            {3,9,1,8 }, //room 2 hallwayp1
+            {7,2,4,5 }, //room 3 hallwayp2
+            {0,0,3,0 }, //room 4 kitchen
+            {0,6,3,0 }, //room 5 basement
+            {5,0,0,0 }, //room 6 bathroom
+            {0,3,0,0 }, //room 7 living room
+            {0,0,0,2 }, //room 8 bedroom2
+            {2,10,0,0 }, //room 9 outside
+            {9,0,0,0 } //room 10 shed
         };
     //this is the info presented to the player as they navigate rooms
     public string[] roomInfo =
