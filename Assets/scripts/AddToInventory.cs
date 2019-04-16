bbdf67468c2c10a7f9bbf14669f8179ab0fda059
Kyrem13 <48100776+Kyrem13@users.noreply.github.com>
2019-04-16 15:43:48 -0400

    stuff

---
+++ Assets/scripts/AddToInventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddToInventory : MonoBehaviour
+{
+    public GameObject theGameController;
+    public GameControlScript theControllerScript;
+    public Text myText;
+    public Text newItem;
+    public GameObject myGameObject;
+    public GameObject invGameObject;
+    public string theInventoryItem = "Inventory";
+    public string numToString;
+    public int addedItem;
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
