2c79508945c0cf28ad0d14b4d0f8822dd205243e
Kyrem13 <48100776+Kyrem13@users.noreply.github.com>
2019-04-18 12:53:36 -0400

    Put in the AddToInventory Script

--- Assets/scripts/AddToInventory.cs
+++ Assets/scripts/AddToInventory.cs
@@ -17,6 +17,33 @@ public class AddToInventory : MonoBehaviour
 
     private void OnMouseDown()
     {
-        
+        theGameController = GameObject.Find("GameController");
+        theControllerScript = theGameController.GetComponent<GameControlScript>();
+        myGameObject = GameObject.Find("Canvas/Tell the Player");
+        myText = myGameObject.GetComponent<Text>();
+
+        if (this.tag == "I")
+        {
+            if (theControllerScript.InventoryList.Count < 10)
+            {
+                theControllerScript.InventoryList.Add(name);
+                addedItem = theControllerScript.InventoryList.Count;
+                Destroy(gameObject);
+
+                numToString = addedItem.ToString();
+                theInventoryItem += numToString;
+                invGameObject = GameObject.Find("Canvas/Vertical Panel/" + theInventoryItem);
+                newItem = invGameObject.GetComponent<Text>();
+                newItem.text = name;
+            }
+            else
+            {
+                myText.text += "\nYo man, I think I can only carry 10 things in these small pockets of mine. I feel like maybe I can get rid of them by shoving them into some puzzles. Am I seriously gonna have to break the 4th wall again? I'm just ripping off Dead Pool at this point.";
+            }
+        }
+        else
+        {
+            myText.text += "\nThe " + this.name + " cannot be put in inventory";
+        }
     }
 }
