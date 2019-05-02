386547adf24c7c18d3af44a0edaba3697a2e6a72
cmaurolccc <48099019+cmaurolccc@users.noreply.github.com>
2019-05-02 18:26:09 -0400

    Update StartButton.cs

--- Assets/scripts/StartButton.cs
+++ Assets/scripts/StartButton.cs
@@ -2,18 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//Code used for the start button in the main menu. Made by Chris.
 
 public class StartButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Loads Room 1 (Bedroom)
     private void OnMouseDown()
     {
         SceneManager.LoadScene("room 1");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
