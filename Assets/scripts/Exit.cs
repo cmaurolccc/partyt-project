a3a04258d7cfa208cd1d0a8b2adcb7ee8b137927
cmaurolccc <48099019+cmaurolccc@users.noreply.github.com>
2019-05-02 18:27:35 -0400

    Update Exit.cs

--- Assets/scripts/Exit.cs
+++ Assets/scripts/Exit.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Exits the game. Made by Chris.
+
 public class Exit : MonoBehaviour
-{
-    // Start is called before the first frame update
+{   //This makes it so that the game exits when you click the Exit button.
     private void OnMouseDown()
     {
         Application.Quit();
